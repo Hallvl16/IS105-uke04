4f426a86c4df159ba41150994f135675ecbbfd26
Hallvl16 <Hallvl16@student.uia.no>
2017-01-26 14:17:13 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package main
 
 import "fmt"
+       "time"
 
 func main() {
 	fmt.Println("Hello")
 	fmt.Println("Is it me you're looking for?")
 	fmt.Println("bye forever")
+	
+	fmt.Println("The time is", time.Now())
+	
 }
